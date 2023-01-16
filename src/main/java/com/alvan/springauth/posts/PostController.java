211696c8e777b4b8141feb36e2bf017e74f995d9
alvannwanorim <alvannwanorim@gmail.com>
2023-01-17 00:29:50 +0100

    Post endpoints setup

---
+++ src/main/java/com/alvan/springauth/posts/PostController.java
@@ -0,0 +1,10 @@
+package com.alvan.springauth.posts;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/posts")
+public class PostController {
+    
+}
---
+++ src/main/java/com/alvan/springauth/posts/PostService.java
@@ -0,0 +1,8 @@
+package com.alvan.springauth.posts;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostService {
+    
+}
